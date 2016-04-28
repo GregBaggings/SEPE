4eae8b62a6665a58801a9faf744eb4477ef4dea9
Gergely Agnecz <agnegerg@gmail.com>
2016-04-28 23:44:43 +0200

    test fix

--- src/test/java/integrationTests/AddCarIT.java
+++ src/test/java/integrationTests/AddCarIT.java
@@ -38,10 +38,6 @@ public class AddCarIT {
 		driver.findElement(By.name("password")).sendKeys("admin");
 		driver.findElement(By.name("submit")).click();
 		driver.findElement(By.linkText("Add Car product")).click();
-		driver.findElement(By.id("name")).sendKeys("Test Car");
-		driver.findElement(By.id("age")).clear();
-		driver.findElement(By.id("age")).sendKeys("1");
-		driver.findElement(By.id("price")).sendKeys("1000");
 		driver.findElement(By.id("addCar")).click();
 		Assert.assertTrue(driver.getCurrentUrl().equals("http://localhost:8080/CW/admin/addCar"));
 		driver.quit();
